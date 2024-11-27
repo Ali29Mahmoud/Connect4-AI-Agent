60ee9e47f5e7203e631113ebd5d7d794fcf232f3
habibatarek26 <147947993+habibatarek26@users.noreply.github.com>
2024-11-27 15:20:26 +0200

    getChildern imple

--- minmax.py
+++ minmax.py
@@ -1,8 +1,12 @@
-import math
+# from screens.assets.guiAssets import *
+# from screens.game_screen import check_connected_4, player_scores
+
+user =1
+comp =2
 def minimize(state):
     if terminalTest(state):
         return eval(state)
-    minChild , minUtility = None , math.inf
+    minChild , minUtility = None , 111111111
     for child in getChildren(state):
         temp , utility= maximize(child)
         if utility < minUtility:
@@ -12,7 +16,7 @@ def minimize(state):
 def maximize(state):
     if terminalTest(state):
         return  eval(state)
-    maxChild , maxUtility = None , -math.inf
+    maxChild , maxUtility = None , -11111111
     for child in getChildren(state):
         temp , utility = minimize(state)
         if utility > maxUtility:
@@ -22,7 +26,7 @@ def maximize(state):
 def minimizeWithPruning(state , alpha , beta):
     if terminalTest(state):
         return eval(state)
-    minChild , minUtility = None , math.inf
+    minChild , minUtility = None ,  1111111111
     for child in getChildren(state):
         temp , utility= maximizeWithPruning(child)
         if utility < minUtility:
@@ -36,7 +40,7 @@ def minimizeWithPruning(state , alpha , beta):
 def maximizeWithPruning(state , alpha , beta):
     if terminalTest(state):
         return  eval(state)
-    maxChild , maxUtility = None , -math.inf
+    maxChild , maxUtility = None , -11111111
     for child in getChildren(state):
         temp , utility = minimizeWithPruning(state)
         if utility > maxUtility:
@@ -48,8 +52,17 @@ def maximizeWithPruning(state , alpha , beta):
     return maxChild , maxUtility
 
 def terminalTest(state):
-    return 0
+    return 0 #check_connected_4(state,comp)-check_connected_4(state,user)
 def eval(state):
     return 0
-def getChildren():
-    return None
+def getChildren(board,player):
+    children = []
+    for col in range(7):
+        for row in range(5, -1, -1):
+            index = row * 7 + col
+            if board[index] == '0':
+                new_board = list(board)
+                new_board[index] = str(player)
+                children.append(''.join(new_board))
+                break
+    return children
